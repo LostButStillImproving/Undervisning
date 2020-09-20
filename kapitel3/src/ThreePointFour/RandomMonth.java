7793293bb4f8dd373f71ad347399399896820655
LostButStillImproving <mike6353@edu.easj.dk>
2020-09-20 16:00:07 +0200

    Added description comment

--- kapitel3/src/ThreePointFour/RandomMonth.java
+++ kapitel3/src/ThreePointFour/RandomMonth.java
@@ -1,6 +1,12 @@
 package ThreePointFour;
 import java.time.Month;
 import java.util.Random;
+/*
+**3.4
+(Random month) Write a program that randomly generates an integer between 1
+and 12 and displays the English month names January, February, . . . , December
+for the numbers 1, 2, . . . , 12, accordingly.
+*/
 
 public class RandomMonth {
     public static void main(String[] args) {
