9df2d4a1d8f0c7b926036ba08b7f875998dd7e37
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-10-11 14:44:51 +0200

    SixPointFive finished with a small test

--- kapitel6/src/SixPointFive/Methods.java
+++ kapitel6/src/SixPointFive/Methods.java
@@ -1,5 +1,15 @@
 package SixPointFive;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Methods {
-    public void displaySortedNumbers(double num1, double num2, double num3){}
+    public List<Double> displaySortedNumbers(double a, double b, double c){
+
+        List<Double> list = Arrays.asList(a,b,c);
+        List<Double> sortedList = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        return sortedList;
+    }
 }
