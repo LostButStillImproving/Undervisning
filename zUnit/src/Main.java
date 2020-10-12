ee982b2893fa33c6178fb15ebde5a4cd578109f9
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-10-12 12:24:55 +0200

    Removed redundant imports

--- zUnit/src/Main.java
+++ zUnit/src/Main.java
@@ -1,6 +1,4 @@
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
