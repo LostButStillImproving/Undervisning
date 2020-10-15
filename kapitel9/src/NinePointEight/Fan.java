0c1ccecc3df0b7e48219c7c3e19cd78985d0dc9d
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-10-15 18:18:59 +0200

    9.8

--- kapitel9/src/NinePointEight/Fan.java
+++ kapitel9/src/NinePointEight/Fan.java
@@ -1,13 +1,13 @@
 package NinePointEight;
 
 public class Fan {
-    final int SLOW = 1;
-    final int MEDIUM = 2;
-    final static int FAST = 3;
+    public final static int SLOW = 1;
+    public final static int MEDIUM = 2;
+    public final static int FAST = 3;
 
     private int speed;
     private Boolean on = false;
-    private Double radius = 5.0;
+    private Double radius;
     String colour = "blue";
 
     Fan(int speed, String colour, double radius) {
@@ -21,6 +21,10 @@ public class Fan {
 
     public void setOn() {
         this.on = true;
+    }
+
+    public void setSpeed(int speed){
+        this.speed = speed;
     }
 
     public Double getRadius() {
@@ -47,15 +51,20 @@ public class Fan {
     }
 
     public static void main(String[] args) {
-        Fan fan = new Fan(3,"yellow", 10.);
-        Fan fan1 = new Fan(2, "blue", 5.);
+        Fan fan = new Fan(SLOW,"yellow", 10.);
+        Fan fan1 = new Fan(FAST, "blue", 5.);
+        Fan fan2 = new Fan(MEDIUM, "blue", 3.);
+
         System.out.println(fan.toString());
         System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
         fan.setOn();
         fan1.setOn();
+        fan2.setOn();
+        fan.setSpeed(FAST);
         System.out.println(fan.toString());
         System.out.println(fan1.toString());
-        System.out.println();
+        System.out.println(fan2.toString());
     }
 
 }
