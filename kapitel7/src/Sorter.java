4bc121bae8fe8ada9b61afcd31cf7943892601a2
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-10-26 16:00:55 +0100

    Refactored method

--- kapitel7/src/Sorter.java
+++ kapitel7/src/Sorter.java
@@ -1,14 +1,11 @@
 import java.util.stream.Stream;
 
+import static java.lang.String.join;
+
 public class Sorter {
     public static String sort(String string) {
         Stream<String> stringStream = Stream.of(string.split(""));
-        String[] reversed = stringStream.sorted().toArray(String[]::new);
-        StringBuilder reversedStringBuilt = new StringBuilder();
-        for (String s : reversed) {
-            reversedStringBuilt.append(s);
-        }
-        String reversedString = reversedStringBuilt.toString();
-        return reversedString;
+        String reversed = join("", stringStream.sorted().toArray(String[]::new));
+        return reversed;
     }
 }
