07aadae48879933e83beed2a4ac5c45d3ccdfbe2
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-10-28 15:56:08 +0100

    added printing of path

--- kapitel7/src/SevenPointThirtySeven/BeanMachine.java
+++ kapitel7/src/SevenPointThirtySeven/BeanMachine.java
@@ -10,6 +10,8 @@ public class BeanMachine {
     private int ballCount;
     private int[] slots;
 
+    private String paths = "";
+
     private int dertermineSlot() {
 
         int countRIGHT = 0;
@@ -17,8 +19,9 @@ public class BeanMachine {
         for (int i = 0; i < slots.length - 1; i++) {
 
             if (random.nextBoolean()) {
+                paths +=  "R";
                 countRIGHT++;
-            }
+            } else paths += "L";
         }
         return countRIGHT;
     }
@@ -27,6 +30,7 @@ public class BeanMachine {
         System.out.println("How many slots are there?");
         var slotCount = scan.nextInt();
         this.slots = new int[slotCount];
+
 
     }
     private void howManyBalls(){
@@ -37,11 +41,14 @@ public class BeanMachine {
     private void increaseSlotBallCount(int slotToBeIncreased){
         slots[slotToBeIncreased] += 1;
     }
-    private void printResult() {
-        for (int slot : slots) {
-            System.out.print("  " + slot + "  ");
-        }
+
+  private void printResult() {
+      for (int i = 1; i < paths.length() + 1 ; i++) {
+        if (i % (slots.length - 1)  == 0) {
+            System.out.println(paths.charAt(i - 1) + "\n");
+        } else System.out.print(paths.charAt(i - 1));
     }
+  }
     public void startGame(){
 
         howManySlots();
